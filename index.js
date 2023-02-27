747567adaffbc8ba6eac55a4feb9aa7e1c71166c
Aleksandr Shevkunenko <alex.4everyours@gmail.com>
2023-02-27 22:52:53 +0500

    index.js created

---
+++ index.js
@@ -0,0 +1,10 @@
+import Validator from './src/Validator.js';
+import StringSchema from './src/StringSchema.js';
+import NumberSchema from './src/NumberSchema.js';
+import ArraySchema from './src/ArraySchema.js';
+import ObjectSchema from './src/ObjectSchema.js';
+
+// prettier-ignore
+export {
+  Validator, StringSchema, NumberSchema, ArraySchema, ObjectSchema,
+};
